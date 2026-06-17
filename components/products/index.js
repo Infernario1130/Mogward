ed5e6c17f0cabb7d785ec0a81e1f6457b3eacc79
Infernario1130 <infernario11@gmail.com>
2026-06-18 05:23:19 +0530

    feat: Add new products

--- components/products/index.js
+++ components/products/index.js
@@ -1,8 +1,10 @@
 import MogwardProtocol from "./ProductArtifact1";
+import SkinProtocol from "./ProductArtifact2";
+import FrameProtocol from "./ProductArtifact3";
 
 export const productMap = {
   "6a2d29efa993b7307f224524": MogwardProtocol,
    //"6a2d29efa993b7307f224525": DietProtocol,
-  // "6a2d29efa993b7307f224526": SkinProtocol,
-  // "6a2d29efa993b7307f224527": FrameProtocol,
+   "6a2d29efa993b7307f224526": SkinProtocol,
+   "6a2d29efa993b7307f224527": FrameProtocol,
 };
